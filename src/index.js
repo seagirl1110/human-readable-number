856dcd75b1907b5fda8ca26a2728b40d0186e1ce
Marina Elizarova <seagirl1110@gmail.com>
2022-01-09 15:25:28 +0300

    feat: solution of task 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = function toReadable(number) {
     const numberColl = number.toString().split('');
     const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const numTeen = { 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 15: 'fifteen', 18: 'eighteen'};
+    const numTens = { 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 80: 'eighty'};
 
     if (numberColl.length === 1) {
         return num[numberColl[0]];
@@ -8,66 +10,17 @@ module.exports = function toReadable(number) {
 
     if (numberColl.length === 2) {
         if (numberColl[0] === '1') {
-            if (numberColl[1] === '0') {
-                return `ten`;
+            if (typeof (numTeen[+numberColl[1] + 10]) !== 'undefined') {
+                return numTeen[+numberColl[1] + 10];
             }
-
-            if (numberColl[1] === '1') {
-                return `eleven`;
-            }
-
-            if (numberColl[1] === '2') {
-                return `twelve`;
-            }
-
-            if (numberColl[1] === '3') {
-                return `thirteen`;
-            }
-
-            if (numberColl[1] === '5') {
-                return `fifteen`;
-            }
-
-            if (numberColl[1] === '8') {
-                return `${num[numberColl[1]]}een`;
-            }
-
             return `${num[numberColl[1]]}teen`;
         }
 
-        if (numberColl[0] === '2') {
+        if (typeof (numTens[numberColl[0] + '0']) !== 'undefined') {
             if (numberColl[1] === '0') {
-                return `twenty`;
+                return numTens[numberColl[0] + '0'];
             }
-            return `twenty ${num[numberColl[1]]}`;
-        }
-
-        if (numberColl[0] === '3') {
-            if (numberColl[1] === '0') {
-                return `thirty`;
-            }
-            return `thirty ${num[numberColl[1]]}`;
-        }
-
-        if (numberColl[0] === '4') {
-            if (numberColl[1] === '0') {
-                return `forty`;
-            }
-            return `forty ${num[numberColl[1]]}`;
-        }
-
-        if (numberColl[0] === '5') {
-            if (numberColl[1] === '0') {
-                return `fifty`;
-            }
-            return `fifty ${num[numberColl[1]]}`;
-        }
-
-        if (numberColl[0] === '8') {
-            if (numberColl[1] === '0') {
-                return `${num[numberColl[0]]}y`;
-            }
-            return `${num[numberColl[0]]}y ${num[numberColl[1]]}`;
+            return `${numTens[numberColl[0] + '0']} ${num[numberColl[1]]}`;
         }
 
         if (numberColl[1] === '0') {
@@ -81,71 +34,21 @@ module.exports = function toReadable(number) {
             if (numberColl[2] === '0') {
                 return `${num[numberColl[0]]} hundred`;
             }
-
             return `${num[numberColl[0]]} hundred ${num[numberColl[2]]}`;
         }
 
         if (numberColl[1] === '1') {
-            if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred ten`;
+            if (typeof (numTeen[+numberColl[2] + 10]) !== 'undefined') {
+                return `${num[numberColl[0]]} hundred ${numTeen[+numberColl[2] + 10]}`;
             }
-
-            if (numberColl[2] === '1') {
-                return `${num[numberColl[0]]} hundred eleven`;
-            }
-
-            if (numberColl[2] === '2') {
-                return `${num[numberColl[0]]} hundred twelve`;
-            }
-
-            if (numberColl[2] === '3') {
-                return `${num[numberColl[0]]} hundred thirteen`;
-            }
-
-            if (numberColl[2] === '5') {
-                return `${num[numberColl[0]]} hundred fifteen`;
-            }
-
-            if (numberColl[2] === '8') {
-                return `${num[numberColl[0]]} hundred ${num[numberColl[2]]}een`; 
-            }
-
             return `${num[numberColl[0]]} hundred ${num[numberColl[2]]}teen`; 
         }
 
-        if (numberColl[1] === '2') {
+        if (typeof (numTens[numberColl[1] + '0']) !== 'undefined') {
             if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred twenty`;
+                return `${num[numberColl[0]]} hundred ${numTens[numberColl[1] + '0']}`;
             }
-            return `${num[numberColl[0]]} hundred twenty ${num[numberColl[2]]}`; 
-        }
-
-        if (numberColl[1] === '3') {
-            if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred thirty`;
-            }
-            return `${num[numberColl[0]]} hundred thirty ${num[numberColl[2]]}`; 
-        }
-
-        if (numberColl[1] === '4') {
-            if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred forty`;
-            }
-            return `${num[numberColl[0]]} hundred forty ${num[numberColl[2]]}`; 
-        }
-
-        if (numberColl[1] === '5') {
-            if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred fifty`;
-            }
-            return `${num[numberColl[0]]} hundred fifty ${num[numberColl[2]]}`; 
-        }
-
-        if (numberColl[1] === '8') {
-            if (numberColl[2] === '0') {
-                return `${num[numberColl[0]]} hundred ${num[numberColl[1]]}y`;
-            }
-            return `${num[numberColl[0]]} hundred ${num[numberColl[1]]}y ${num[numberColl[2]]}`;            
+            return `${num[numberColl[0]]} hundred ${numTens[numberColl[1] + '0']} ${num[numberColl[2]]}`; 
         }
     
         if (numberColl[2] === '0') {
